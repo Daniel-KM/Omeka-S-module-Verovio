619cb2ada70c01d3234584f57689b122d83f8571
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-05-19 00:00:00 +0000

    Released version 3.4.8 for Omeka S v4.1.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -39,3 +39,125 @@ if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActi
     );
     throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
 }
+
+if (version_compare($oldVersion, '3.4.8', '<')) {
+    $this->updateWhitelist();
+
+    /**
+     * Migrate blocks of this module to new blocks of Omeka S v4.1.
+     *
+     * Replace filled settting "heading" by a specific block "Heading".
+     * Move setting template to block layout template.
+     *
+     * @see \Omeka\Db\Migrations\MigrateBlockLayoutData
+     */
+
+    $pageRepository = $entityManager->getRepository(\Omeka\Entity\SitePage::class);
+
+    $viewHelpers = $services->get('ViewHelperManager');
+    $escape = $viewHelpers->get('escapeHtml');
+    $hasBlockPlus = $this->isModuleActive('BlockPlus');
+
+    $newTemplates = [
+        'verovio' => 'verovio',
+        'verovio-toolkit' => 'verovio-toolkit',
+        'verovio-mei-viewer' => 'verovio-toolkit-bootstrap-3',
+        'verovio-viewer' => 'verovio-toolkit-bootstrap-4',
+    ];
+
+    $pagesUpdated = [];
+    $pagesUpdated2 = [];
+    foreach ($pageRepository->findAll() as $page) {
+        $pageSlug = $page->getSlug();
+        $siteSlug = $page->getSite()->getSlug();
+        $position = 0;
+        foreach ($page->getBlocks() as $block) {
+            $block->setPosition(++$position);
+            $layout = $block->getLayout();
+            if ($layout !== 'verovio') {
+                continue;
+            }
+            $data = $block->getData() ?: [];
+
+            $heading = $data['heading'] ?? '';
+            if (strlen($heading)) {
+                $b = new \Omeka\Entity\SitePageBlock();
+                $b->setPage($page);
+                $b->setPosition(++$position);
+                if ($hasBlockPlus) {
+                    $b->setLayout('heading');
+                    $b->setData([
+                        'text' => $heading,
+                        'level' => 2,
+                    ]);
+                } else {
+                    $b->setLayout('html');
+                    $b->setData([
+                        'html' => '<h2>' . $escape($heading) . '</h2>',
+                    ]);
+                }
+                $entityManager->persist($b);
+                $block->setPosition(++$position);
+                $pagesUpdated[$siteSlug][$pageSlug] = $pageSlug;
+            }
+            unset($data['heading']);
+            $data['variant'] = 'wasm';
+            $data['version'] = 'local';
+
+            $template = $data['template'] ?? null;
+            if ($template) {
+                $layoutData = $block->getLayoutData();
+                $layoutData['template_name'] = $newTemplates[pathinfo($template, PATHINFO_FILENAME)]
+                    ?? pathinfo($template, PATHINFO_FILENAME);
+                $block->setLayoutData($layoutData);
+                $pagesUpdated2[$siteSlug][$pageSlug] = $pageSlug;
+            }
+            unset($data['template']);
+
+            $block->setData($data);
+        }
+    }
+
+    $entityManager->flush();
+    $entityManager->clear();
+
+    if ($pagesUpdated) {
+        $result = array_map('array_values', $pagesUpdated);
+        $message = new PsrMessage(
+            'The setting "heading" was removed from block Verovio. New blocks "Heading" or "Html" were prepended to all blocks that had a filled heading. You may check pages for styles: {json}', // @translate
+            ['json' => json_encode($result, 448)]
+        );
+        $messenger->addWarning($message);
+        $logger->warn($message->getMessage(), $message->getContext());
+    }
+
+    if ($pagesUpdated2) {
+        $result = array_map('array_values', $pagesUpdated2);
+        $message = new PsrMessage(
+            'The setting "template" was moved to the new block layout settings available since Omeka S v4.1. You may check pages for styles: {json}', // @translate
+            ['json' => json_encode($result, 448)]
+        );
+        $messenger->addWarning($message);
+        $logger->warn($message->getMessage(), $message->getContext());
+
+        $message = new PsrMessage(
+            'The template files for the block Verovio should be moved from "view/common/block-layout" to "view/common/block-template" in your themes. You may check your themes for pages: {json}', // @translate
+            ['json' => json_encode($result, 448)]
+        );
+        $messenger->addError($message);
+        $logger->warn($message->getMessage(), $message->getContext());
+    }
+
+    $siteSettings = $services->get('Omeka\Settings\Site');
+    $siteIds = $api->search('sites', [], ['returnScalar' => 'id'])->getContent();
+    foreach ($siteIds as $siteId) {
+        $siteSettings->setTargetId($siteId);
+        $siteTemplate = basename($siteSettings->get('verovio_template', 'common/verovio') ?: 'common/verovio');
+        $siteSettings->set('verovio_template', 'common/' . ($newTemplates[$siteTemplate] ?? $siteTemplate));
+    }
+
+    $message = new PsrMessage(
+        'A new option allows to use the version of Verovio provided online.' // @translate
+    );
+    $messenger->addWarning($message);
+}
